4d4684d221f96d0c30984ad44477d0e088b0bc4c
acik92 <minacik9492@gmail.com>
2025-11-14 15:29:18 +0800

    Update acik-smart.ps1

--- acik-smart.ps1
+++ acik-smart.ps1
@@ -13,12 +13,12 @@ New-Item -ItemType Directory -Force -Path $dl | Out-Null
 $apps = @(
     @{name="Google Chrome"; process="chrome"; url="https://dl.google.com/chrome/install/latest/chrome_installer.exe"; args="/silent /install"},
     @{name="Mozilla Firefox"; process="firefox"; url="https://download.mozilla.org/?product=firefox-latest&os=win64&lang=en-US"; args="/S"},
-    @{name="VLC Media Player"; process="vlc"; url="https://get.videolan.org/vlc/3.0.21/win64/vlc-3.0.21-win64.exe"; args="/S"},
+    @{name="VLC Media Player"; process="vlc"; url="https://mirror-hk.koddos.net/videolan/vlc/3.0.21/win64/vlc-3.0.21-win64.exe"; args="/S"},
     @{name="TeamViewer"; process="TeamViewer"; url="https://download.teamviewer.com/download/TeamViewer_Setup_x64.exe"; args="/S"},
     @{name="WinRAR"; process="winrar"; url="https://www.rarlab.com/rar/winrar-x64-701.exe"; args="/S"},
-    @{name="Adobe Acrobat Reader"; process="AcroRd32"; url="https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2400820463/AcroRdrDCx642400820463_MUI.exe"; args="/sAll /rs /rps /msi EULA_ACCEPT=YES ENABLE_CHROMEEXT=0"},
-    @{name="WhatsApp"; process="WhatsApp"; url="https://web.whatsapp.com/desktop/windows/release/x64/WhatsAppSetup.exe"; args="/S"},
-    @{name="Telegram"; process="Telegram"; url="https://updates.tdesktop.com/tsetup-x64.5.9.1.exe"; args="/S"}
+    @{name="Adobe Acrobat Reader"; process="AcroRd32"; url="https://get.adobe.com/reader/download?os=Windows+10&name=Reader+2025.001.20844+English+for+Windows+%28Recommended%29&lang=en&nativeOs=Windows+10&accepted=&declined=mss%2Ccr%2Chrm&preInstalled=&site=otherversions"; args="/sAll /rs /rps /msi EULA_ACCEPT=YES ENABLE_CHROMEEXT=0"},
+    @{name="WhatsApp"; process="WhatsApp"; url="https://get.microsoft.com/installer/download/9NKSQGP7F2NH?cid=website_cta_psi"; args="/S"},
+    @{name="Telegram"; process="Telegram"; url="https://td.telegram.org/tx64/tsetup-x64.6.2.4.exe"; args="/S"}
 )
 
 # Semak dan pasang
